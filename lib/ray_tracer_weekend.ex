f25e9e85b3d651a2c7214b6373f50f26e490ab39
kokoax <yomo4808@gmail.com>
2018-01-31 20:21:46 +0900

    Fix Vec3に合わせて簡略化

--- lib/ray_tracer_weekend.ex
+++ lib/ray_tracer_weekend.ex
@@ -2,23 +2,31 @@ defmodule RayTracerWeekend do
   @nx 200
   @ny 100
 
+  def product([ir, ig, ib]) do
+    "#{ir} #{ig} #{ib}\n"
+  end
+
+  def to_image(rgbs) do
+     rgb_str = rgbs
+      |> Enum.concat()
+      |> Enum.map(fn(rgb) ->
+        rgb |> product()
+      end)
+      |> Enum.join()
+    "P3\n#{@nx} #{@ny}\n255\n" <> rgb_str
+  end
+
   def main(_opts) do
-    "P3\n#{@nx} #{@ny}\n255" |> IO.puts
     0..(@ny-1) |> Enum.map(fn(j) ->
       0..(@nx-1) |> Enum.map(fn(i) ->
-        r = i / @nx
-        g = (@ny-j-1) / @ny
-        b = 0.2
-        ir = round(255.99*r)
-        ig = round(255.99*g)
-        ib = round(255.99*b)
-        "#{ir} #{ig} #{ib}"
+        col = Vec3.new(i/@nx, (@ny-j-1)/@ny, 0.2)
+        ir = round(255.99*(col.r))
+        ig = round(255.99*(col.g))
+        ib = round(255.99*(col.b))
+        [ir, ig, ib]
       end)
     end)
-    |> Enum.map(fn(items) ->
-      items |> Enum.map(fn(rgb) ->
-        rgb |> IO.puts
-      end)
-    end)
+    |> to_image
+    |> IO.puts
   end
 end
